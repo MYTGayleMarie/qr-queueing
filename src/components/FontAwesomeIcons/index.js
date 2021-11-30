584fcb98362813d0c67df25cd43dd94f2d3714a6
Gwyneth Chiu <gwynethchiu@Gwyneths-MacBook-Air.local>
2021-11-30 23:01:30 +0800

    Finished login page

--- src/components/FontAwesomeIcons/index.js
+++ src/components/FontAwesomeIcons/index.js
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faEyeSlash, faSearch, faTimes, faBars, faUser, faLock, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faEye,faEyeSlash, faSearch, faTimes, faBars, faUser, faLock, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faEyeSlash, faSearch, faTimes, faBars, faUser, faLock, faCheckSquare, faCoffee)
+library.add( faEye, faEyeSlash, faSearch, faTimes, faBars, faUser, faLock, faCheckSquare, faCoffee)
--- src/components/view/Login/Login.js
+++ src/components/view/Login/Login.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
+import reactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 //css
@@ -9,14 +10,21 @@ import clinic from '../../../images/clinic.png';
 import logo  from '../../../images/logo.png';
 
 function Login() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePassword = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <>
         <div class="container">
         <div class="row">
-            <div class="col-sm d-flex justify-content-center">
+            <div class="col-lg d-flex justify-content-center">
                 <img src={clinic} alt={"clinic"} class="login-clinic"></img>
             </div>
-            <div class="col-sm d-flex justify-content-center">
+            <div class="col-lg d-flex justify-content-center">
                 <div class="login-container">
                     <div class="row">
                         <div class="col-sm-12">
@@ -34,14 +42,25 @@ function Login() {
                         </div>
                     </div>
                     <div class="row">
-                    <label for="details" class="form-label">Enter Details</label><br />
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" /><br />
-                        <div class="input-group" id="show_hide_password">
-                            <input class="form-control" type="password" placeholder="Password"/>
-                            <div class="input-group-addon">
-                                <a href=""><FontAwesomeIcon icon="eye-slash" class="hide-icon"/></a>
+                    <label for="details" class="form-label details-label">Enter Details</label><br />
+                       <div class="username-wrapper">
+                            <input type="text" class="login-input" id="username" name="username" placeholder="Username" /><br />
+                       </div>
+                       <div class="password-wrapper">
+                            <input class="login-input" type={showPassword ? "text" : "password"} placeholder="Password"/>
+                            <div class="eye-icon">
+                                <FontAwesomeIcon icon={showPassword ? "eye" : "eye-slash"} alt={"eye"} aria-hidden="true" onClick={togglePassword}/>
                             </div>
-                            {/* <FontAwesomeIcon icon='eye-slash' class="input-icon"/> */}
+                       </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 d-flex justify-content-end">
+                            <span class="forgot-pass">Forgot Password?</span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 d-flex justify-content-center">
+                        <button type="button" class="login-btn">Login</button>
                         </div>
                     </div>
                 </div>
