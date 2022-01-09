cc0a4e0d77e48eda0ac0c547666536d99012d583
Cyrus David Pastelero <cyrus@mytdevcorp.com>
2022-01-09 11:03:19 +0800

    Navbar and redirection

--- src/components/view/Login/Login.js
+++ src/components/view/Login/Login.js
@@ -1,123 +1,155 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import { Navigate } from 'react-router';
-import { refreshPage } from "../../../utilities/Common";
+import { refreshPage, getRoleId } from '../../../utilities/Common';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 //css
-import './Login.css'
+import './Login.css';
 
 //images
 import clinic from '../../../images/clinic.png';
-import logo  from '../../../images/logo.png';
-import axios from "axios";
+import logo from '../../../images/logo.png';
+import axios from 'axios';
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [showPassword, setShowPassword] = useState(false);
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-    const togglePassword = () => {
-        setShowPassword(!showPassword);
-    };
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
 
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-    });
+  let NavigationRedirection;
 
-    function submit(e) {
-        e.preventDefault();
-        axios({
-            method: 'post',
-            url: window.$link + 'login',
-            withCredentials: false, 
-            params: {
-                api_key: window.$api_key,
-                email: data.email,
-                password: data.password
-            }
-        }).then(function (response) {
-            localStorage.setItem('token', JSON.stringify(response.data.token));
-            localStorage.setItem('user', JSON.stringify(response.data.id));
-            localStorage.setItem('role_id', JSON.stringify(response.data.role_id));
-            refreshPage();
-        }).catch(function (error) {
-            toast.error("Invalid Login");
-        });
+  function submit(e) {
+    e.preventDefault();
+    axios({
+      method: 'post',
+      url: window.$link + 'login',
+      withCredentials: false,
+      params: {
+        api_key: window.$api_key,
+        email: data.email,
+        password: data.password,
+      },
+    })
+      .then(function (response) {
+        localStorage.setItem('token', JSON.stringify(response.data.token));
+        localStorage.setItem('user', JSON.stringify(response.data.id));
+        refreshPage();
+      })
+      .catch(function (error) {
+        toast.error('Invalid Login');
+      });
+  }
+
+  function handle(e) {
+    const newData = { ...data };
+    setData(newData);
+    newData[e.target.id] = e.target.value;
+  }
+
+  if (window.$userToken != null) {
+    if (window.$userId === '5') {
+      NavigationRedirection = <Navigate to="/cashier" />;
+    } else {
+      NavigationRedirection = <Navigate to="/registration" />;
     }
 
-    function handle(e) {
-        const newData= {...data}
-        newData[e.target.id] = e.target.value
-        setData(newData)
-    }
+    return NavigationRedirection;
+  }
 
-    if(window.$userToken != null) {
-        return (
-            <Navigate to = "/registration"/>
-        )
-    }
-
-    document.body.style = 'background: white;';
-
-    return (
-        <>
-        <div className="container">
+  document.body.style = 'background: white;';
+  console.log(getRoleId());
+  return (
+    <>
+      <div className="container">
         <div className="row">
-            <div className="col-lg d-flex justify-content-center">
-                <img src={clinic} alt={"clinic"} className="login-clinic"></img>
-            </div>
-            <div className="col-lg d-flex justify-content-center">
-                <div className="login-container">
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <img src={logo} alt={"logo"} className="logo"></img>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h1 className="welcome">Welcome</h1>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h1 className="sign-in-sign">Sign In to Manager</h1>
-                        </div>
-                    </div>
-                    <form onSubmit={(e) => submit(e)}>
-                    <div className="row">
-                    <label for="details" className="form-label details-label">Enter Details</label><br />
-                       <div className="username-wrapper">
-                            <input onChange= {(e) => handle(e)} id="email" value= {data.email} type="text" className="login-input" id="email" name="email" placeholder="Email" /><br />
-                       </div>
-                       <div className="password-wrapper">
-                            <input onChange= {(e) => handle(e)} id="password" value= {data.password} className="login-input" type={showPassword ? "text" : "password"} placeholder="Password"/>
-                            <div className="eye-icon">
-                                <FontAwesomeIcon icon={showPassword ? "eye" : "eye-slash"} alt={"eye"} aria-hidden="true" onClick={togglePassword}/>
-                            </div>
-                       </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col-sm-12 d-flex justify-content-center">
-                        <button type="submit" className="login-btn">Login</button>
-                        </div>
-                        <ToastContainer hideProgressBar={true}/>
-                    </div>
-                    </form>
+          <div className="col-lg d-flex justify-content-center">
+            <img src={clinic} alt={'clinic'} className="login-clinic"></img>
+          </div>
+          <div className="col-lg d-flex justify-content-center">
+            <div className="login-container">
+              <div className="row">
+                <div className="col-sm-12">
+                  <img src={logo} alt={'logo'} className="logo"></img>
                 </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12">
+                  <h1 className="welcome">Welcome</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-12">
+                  <h1 className="sign-in-sign">Sign In to Manager</h1>
+                </div>
+              </div>
+              <form onSubmit={(e) => submit(e)}>
+                <div className="row">
+                  <label for="details" className="form-label details-label">
+                    Enter Details
+                  </label>
+                  <br />
+                  <div className="username-wrapper">
+                    <input
+                      onChange={(e) => handle(e)}
+                      id="email"
+                      value={data.email}
+                      type="text"
+                      className="login-input"
+                      id="email"
+                      name="email"
+                      placeholder="Email"
+                    />
+                    <br />
+                  </div>
+                  <div className="password-wrapper">
+                    <input
+                      onChange={(e) => handle(e)}
+                      id="password"
+                      value={data.password}
+                      className="login-input"
+                      type={showPassword ? 'text' : 'password'}
+                      placeholder="Password"
+                    />
+                    <div className="eye-icon">
+                      <FontAwesomeIcon
+                        icon={showPassword ? 'eye' : 'eye-slash'}
+                        alt={'eye'}
+                        aria-hidden="true"
+                        onClick={togglePassword}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-sm-12 d-flex justify-content-center">
+                    <button type="submit" className="login-btn">
+                      Login
+                    </button>
+                  </div>
+                  <ToastContainer hideProgressBar={true} />
+                </div>
+              </form>
             </div>
+          </div>
         </div>
-    </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 Login.propTypes = {
-    setToken: PropTypes.func.isRequired
-}
+  setToken: PropTypes.func.isRequired,
+};
 
-export default Login
-
+export default Login;
--- src/components/view/Registration/Registration.js
+++ src/components/view/Registration/Registration.js
@@ -1,7 +1,7 @@
-import React, {Fragment,useState} from 'react';
+import React, { Fragment, useState } from 'react';
 import axios from 'axios';
-import { getToken, getUser, refreshPage  } from '../../../utilities/Common';
-import { useForm } from "react-hooks-helper";
+import { getToken, getUser } from '../../../utilities/Common';
+import { useForm } from 'react-hooks-helper';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -13,17 +13,15 @@ import Header from '../../Header.js';
 import Navbar from '../../Navbar';
 import Table from '../../Table.js';
 
-
 const buttons = ['add-new-patient', 'add-old-patient'];
 const userToken = getToken();
 const userId = getUser();
 var presentDate = new Date();
 
-
 const filterData = {
-    from_date: presentDate,
-    to_date: presentDate,
-    done: false,
+  from_date: presentDate,
+  to_date: presentDate,
+  done: false,
 };
 
 console.log(filterData);
@@ -31,85 +29,81 @@ console.log(filterData);
 var patientData = [];
 
 function Registration() {
-
-    const [filteredData, setFilter] = useForm(filterData);
-    const [render, setRender] = useState([]);
-    React.useEffect(() => {
-        patientData = [];
-        axios({
+  const [filteredData, setFilter] = useForm(filterData);
+  const [render, setRender] = useState([]);
+  React.useEffect(() => {
+    patientData = [];
+    axios({
+      method: 'post',
+      url: window.$link + 'bookings/getAll',
+      withCredentials: false,
+      params: {
+        api_key: window.$api_key,
+        token: userToken.replace(/['"]+/g, ''),
+        requester: userId,
+        date_from: filteredData.from_date,
+        date_to: filteredData.to_date,
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        response.data.bookings.map((booking, index) => {
+          axios({
             method: 'post',
-            url: window.$link + 'bookings/getAll',
-            withCredentials: false, 
+            url: window.$link + 'customers/show/' + booking.customer_id,
+            withCredentials: false,
             params: {
-                api_key: window.$api_key,
-                token: userToken.replace(/['"]+/g, ''),
-                requester: userId,
-                date_from: filteredData.from_date,
-                date_to: filteredData.to_date,
-            }
-        }).then(function (response) {
-            console.log(response);
-            response.data.bookings.map((booking, index) => {
-          
-                axios({
-                    method: 'post',
-                    url: window.$link + 'customers/show/' + booking.customer_id,
-                    withCredentials: false, 
-                    params: {
-                        api_key: window.$api_key,
-                        token: userToken.replace(/['"]+/g, ''),
-                        requester: userId,
-                    }
-                }).then(function (customer) {
-                    var formatBookingTime = new Date(booking.booking_time);
-                    var formatAddedOn = new Date(booking.added_on);
-                    var bookingDetails = {};
-                    bookingDetails.id = booking.id;
-                    bookingDetails.name = customer.data.first_name + " " + customer.data.middle_name + " " + customer.data.last_name;
-                    bookingDetails.bookingTime = formatBookingTime.toDateString();
-                    bookingDetails.serviceType = booking.type;
-                    bookingDetails.addedOn = formatAddedOn.toDateString();
-                    patientData.push(bookingDetails);
-                }).catch(function (error) {
-                    console.log(error);
-                });
-                setRender(patientData);
+              api_key: window.$api_key,
+              token: userToken.replace(/['"]+/g, ''),
+              requester: userId,
+            },
+          })
+            .then(function (customer) {
+              var formatBookingTime = new Date(booking.booking_time);
+              var formatAddedOn = new Date(booking.added_on);
+              var bookingDetails = {};
+              bookingDetails.id = booking.id;
+              bookingDetails.name =
+                customer.data.first_name + ' ' + customer.data.middle_name + ' ' + customer.data.last_name;
+              bookingDetails.bookingTime = formatBookingTime.toDateString();
+              bookingDetails.serviceType = booking.type;
+              bookingDetails.addedOn = formatAddedOn.toDateString();
+              patientData.push(bookingDetails);
+            })
+            .catch(function (error) {
+              console.log(error);
             });
-        }).catch(function (error) {
-            console.log(error);
+          setRender(patientData);
         });
-    },[filteredData]);
-    
-    function filter() {
-    }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [filteredData]);
 
-    return (
-        <div>
-            <Navbar/>
-            <div className="active-cont">
-                <Fragment>
-                <Header 
-                    type='thick'
-                    title='BOOKING MANAGER' 
-                    buttons={buttons} 
-                    tableData={patientData}
-                />
-                <Table
-                    clickable={false}
-                    type={'no-action'}
-                    tableData={patientData}
-                    headingColumns={['BOOKING ID', 'PATIENT NAME', 'BOOKING DATE', 'SERVICE TYPE', 'ADDED ON']}
-                    filteredData={filteredData}
-                    setFilter={setFilter}
-                    filter={filter}
-                    render={setRender}
-                />
-                <ToastContainer hideProgressBar={true}/>
-            
-                </Fragment>
-            </div>
-        </div>
-    )
+  function filter() {}
+
+  return (
+    <div>
+      <Navbar />
+      <div className="active-cont">
+        <Fragment>
+          <Header type="thick" title="BOOKING MANAGER" buttons={buttons} tableData={patientData} />
+          <Table
+            clickable={false}
+            type={'no-action'}
+            tableData={patientData}
+            headingColumns={['BOOKING ID', 'PATIENT NAME', 'BOOKING DATE', 'SERVICE TYPE', 'ADDED ON']}
+            filteredData={filteredData}
+            setFilter={setFilter}
+            filter={filter}
+            render={setRender}
+          />
+          <ToastContainer hideProgressBar={true} />
+        </Fragment>
+      </div>
+    </div>
+  );
 }
 
-export default Registration
+export default Registration;
