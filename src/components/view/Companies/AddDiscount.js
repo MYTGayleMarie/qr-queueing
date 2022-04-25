f15e33db1216aa0b80361f24cc1d298fa719b010
Jhoanna Lagumbay <jhoanna@mytdevcorp.com>
2022-04-25 18:23:32 +0800

    redirect to company discounts manager after adding discount

--- src/components/view/Companies/AddDiscount.js
+++ src/components/view/Companies/AddDiscount.js
@@ -154,9 +154,9 @@ function submit(e, discount) {
         }).then(function (discount_response) {
             console.log(discount_response)
             toast.success("Successfully added company discount details");
-            // setTimeout(function() {
-            //     setRedirect(true);
-            // }, 2000);
+            setTimeout(function() {
+                setRedirect(true);
+            }, 2000);
         }).catch(function (error) {
             toast.error(error);
         })
@@ -169,7 +169,7 @@ function submit(e, discount) {
 
 if(redirect == true) {
     return (
-        <Navigate to = "/companies"/>
+        <Navigate to = "/company-discounts"/>
     )
 }  
 
