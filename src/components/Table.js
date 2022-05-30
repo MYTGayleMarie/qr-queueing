dd4b5d2b512d949f68ab015c6ed830f691988d61
Jhoanna Lagumbay <jhoanna@mytdevcorp.com>
2022-05-30 21:10:34 +0800

    add filter for paid and unpaid in receives

--- src/components/Table.js
+++ src/components/Table.js
@@ -307,7 +307,7 @@ function Table({clickable, type, tableData, headingColumns, breakOn = 'medium', 
     });
  
     //table structure
-    if(type === 'no-action' || type === 'release' || type === 'reports-sales' || type === 'credits' || type === 'receives'||type === 'transaction') {
+    if(type === 'no-action' || type === 'release' || type === 'reports-sales' || type === 'credits'||type === 'transaction') {
 
         const {from_date, to_date, done} = filteredData;
     
@@ -325,6 +325,47 @@ function Table({clickable, type, tableData, headingColumns, breakOn = 'medium', 
                 <div className="col-sm-10 d-flex justify-content-end">
                     <input type="date" className="from-date search" name="from_date" value={from_date} onChange={setFilter} />
                     <input type="date" className="to-date search" name="to_date"  value={to_date} onChange={setFilter} />
+                    <button className="filter-btn" name="done" onClick={setRender != null ? (e) => setRender(!render) : ""}>FILTER</button>
+                </div>
+                </div>
+                <table className={tableClass}>
+                    <thead>
+                        <tr>
+                            {headingColumns.map((col,index) => (
+                                <th key={index}>{col}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data}
+                    </tbody>
+                </table>
+                <TableFooter range={range} slice={slice} setPage={setPage} page={page} footerClass={givenClass} setRowsPerPage={setRowsPerPage} rowsPerPage={rowsPerPage}/>
+             </div>
+        );
+    }
+    else if (type === 'receives') {
+
+        const {from_date, to_date, status} = filteredData;
+    
+        return(
+            <div className="table-container">
+                <div className="search-table-container row">
+
+                <div className="col-sm-2">
+                    {totalCount != null && (
+                        <div className="total-count-container">
+                            <span className="total-count-header-table">TOTAL: </span><span className="total-count-data">{totalCount}</span>
+                        </div>
+                    )}
+                </div>
+                <div className="col-sm-10 d-flex justify-content-end">
+                    <input type="date" className="from-date search" name="from_date" value={from_date} onChange={setFilter} />
+                    <input type="date" className="to-date search" name="to_date"  value={to_date} onChange={setFilter} />
+                     <select name="status" onChange={setFilter}>
+                        <option value="UNPAID" selected>UNPAID</option>
+                        <option value="PAID">PAID</option>
+                    </select>
                     <button className="filter-btn" name="done" onClick={setRender != null ? (e) => setRender(!render) : ""}>FILTER</button>
                 </div>
                 </div>
