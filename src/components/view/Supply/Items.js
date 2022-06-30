277dcc48e016295d8fef8691aed400cc646bc202
Jhoanna Lagumbay <jhoanna@mytdevcorp.com>
2022-06-30 15:38:30 +0800

    fix bug in items

--- src/components/view/Supply/Items.js
+++ src/components/view/Supply/Items.js
@@ -22,10 +22,10 @@ var unit = "";
 
 const buttons = ['add-supply-items'];
 
-const filterData = {
-    from_date: "",
-    to_date: "",
-};
+// const filterData = {
+//     from_date: "",
+//     to_date: "",
+// };
 
 const itemsData = [
     {
@@ -43,12 +43,14 @@ const itemsData = [
 function Items() {
 
     document.body.style = 'background: white;';
-    const [filteredData, setFilter] = useForm(filterData);
+    // const [filteredData, setFilter] = useForm(filterData);
     const [items, setItems] = useState([]);
     const [displayItems, setDisplayItems] = useState([])
     const [inventoryItem, setInventoryItem] = useState("");
     const [inventoryQty, setInventoryQty] = useState("");
     const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [searchName, setSearchName] = useState("")
+    const [searchItem, setSearchItem] = useState(true)
 
     //Inventory Modal
     const [show, setShow] = useState(false);
@@ -58,66 +60,27 @@ function Items() {
     //redirect
     const [redirect, setRedirect] = useState(false);
 
-    React.useEffect(() => {
-        items.length = 0;
-        axios({
-          method: 'post',
-          url: window.$link + 'items/getAll',
-          withCredentials: false,
-          params: {
-            api_key: window.$api_key,
-            token: userToken.replace(/['"]+/g, ''),
-            requester: userId,
-          },
-        }).then(function (response) {
-            // console.log(response.data.items);
-            response.data.items.map((data,index) => {
-                var item = {};
-                item.id = data.item_id;
-                item.item_name = data.item_name;
-                item.item_description = data.description;
-                item.unit=data.default_unit;
-                item.beginning_balance = data.beginning_inventory;
-                item.current_balance = parseFloat(data.qty).toFixed(2);
-                item.remarks = data.remarks;
-
-                setItems(oldArray => [...oldArray, item]);
-                // setDisplayItems(oldArray => [...oldArray, item]);
-            });
-            
-        }).catch(function (error) {
-            console.log(error);
-        });
-    },[]);
-
-
-    // search function
-
-    const [searchName, setSearchName] = useState("")
-    const [searchItem, setSearchItem] = useState(true)
-    // function searchItem(){
-    //   console.log(searchName)
-    // }
-    console.log(searchName)
-    
+ 
     // I use useeffect para no need to call api request everytime naa isearch na items
     React.useEffect(()=>{
-    setDisplayItems([])
       // if wala ray gi search, displayed items will be all the items na na call sa api
       if(searchName===""||searchName===" "){
-        setDisplayItems(items.filter(info=>info.item_name!==null || info.item_name==="") )
+        setDisplayItems([])
+        let tempItems = items
+        setDisplayItems(tempItems)
       } 
       // else, search item using regex
       else {
+        setDisplayItems([])
         // store items to temporary variable para complete gihapon ang tanan items, but filter out null ang name
-        let tempItems = items.filter(info=>info.item_name!==null || info.item_name==="" )
+        let tempItems = items.filter(info=>info.item_name!==null)
         // console.log(tempItems)
         // Create regex using the searched word, lowercase para dili case sensitive ig search
         let searchWord = new RegExp(searchName.toLowerCase())
 
         // filter the temporary items to match the regex, replace double spaces if naa with single space, and transform to lowercase
 
-        let filteredItems = tempItems.filter(info => searchWord.test(info.item_name.replace(/\s\s+/g, ' ').toLowerCase()))
+        let filteredItems = tempItems.filter(info=>searchWord.test(info.item_name.toLowerCase()))
 
         // display filtered Items
         setDisplayItems(filteredItems)
@@ -125,8 +88,42 @@ function Items() {
 
     }, [items, searchName])
 
-    console.log(displayItems)
-    console.log(items)
+
+
+    React.useEffect(()=>{
+      items.length=0
+      axios({
+        method: 'post',
+        url: window.$link + 'items/getAll',
+        withCredentials: false,
+        params: {
+          api_key: window.$api_key,
+          token: userToken.replace(/['"]+/g, ''),
+          requester: userId,
+        },
+      })
+      .then((res)=>{
+        // console.log(res.data.items.sort((a, b) => a.id - b.id))
+        let itemData = res.data.items.filter(info=>info.item_name!==null)
+        let data = itemData.sort((a, b) => a.id - b.id)
+        data.map((data, index)=>{
+          // console.log(data)
+          var item = {}
+            item.id = data.item_id;
+            item.temp = data.item_name+data.item_id
+            item.item_name = data.item_name;
+            item.item_description = data.description;
+            item.unit=data.default_unit.toUpperCase();
+            item.beginning_balance = data.beginning_inventory;
+            item.current_balance = parseFloat(data.qty).toFixed(2);
+            item.remarks = data.remarks;
+
+            setItems(oldArray => [...oldArray, item]);
+        })
+      })
+      .catch((err)=>{console.log(err)})
+    },[])
+
 
     function update(itemId,itemUnit) {
         id = itemId;
@@ -170,16 +167,14 @@ function Items() {
                     </div>
                 </div>
  
-            <Table
-                type={'items'}
-                clickable={true}
-                tableData={displayItems.sort((a,b) => (a.item_name > b.item_name) ? 1 : ((b.item_name > a.item_name) ? -1 : 0))}
-                rowsPerPage={10}
-                headingColumns={['ITEM ID', 'ITEM NAME', 'ITEM DESCRIPTION','UNIT', 'BEGINNING BALANCE', 'CURRENT BALANCE', 'REMARKS', 'ACTION']}
-                filteredData={filteredData}
-                setFilter={setFilter} 
-                link={update}
+            <Table 
+              type={'items'}
+              clickable={true}
+              tableData={displayItems}
+              headingColumns={['ITEM NAME', 'ITEM DESCRIPTION','UNIT', 'BEGINNING BALANCE', 'CURRENT BALANCE', 'REMARKS', 'ACTION']}
+              link={update}
             />
+            
             <br />
             <br />
             </Fragment>
