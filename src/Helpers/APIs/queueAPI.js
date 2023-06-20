a931bc6abb6879cdd95707a6394721e7a57735a9
Vanessa Ibale <vanessa@mytdevcorp.com>
2023-06-20 01:52:08 -0700

    integ: change queue status

---
+++ src/Helpers/APIs/queueAPI.js
@@ -0,0 +1,22 @@
+import { getToken, getUser } from "../../utilities/Common";
+import { getAPICall, postAPICall } from "../AxiosMethodCalls";
+
+export const changeStatus = async (queue_id, status) => {
+  try {
+    const response = await postAPICall(
+      window.$link + "customers/changeQueueStatus",
+
+      {
+        requester: getUser(),
+        api_key: window.$api_key,
+        token: getToken().replace(/['"]+/g, ""),
+        queue_id: queue_id,
+        status: status,
+      }
+    );
+
+    return { data: response.data };
+  } catch (error) {
+    return { error: error.response };
+  }
+};
