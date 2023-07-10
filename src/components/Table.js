27ac6f8ec0ced08550f7eba368a5d99e0a7c0fc0
Vanessa Ibale <vanessa@mytdevcorp.com>
2023-07-10 01:39:10 -0700

    feat: present data for unpaid invoices report if supervisor

--- src/components/Table.js
+++ src/components/Table.js
@@ -1315,6 +1315,7 @@ function Table({
               name="from_date"
               value={from_date}
               onChange={setFilter}
+              disabled={roleId === "12"}
             />
             <input
               type="date"
@@ -1322,11 +1323,13 @@ function Table({
               name="to_date"
               value={to_date}
               onChange={setFilter}
+              disabled={roleId === "12"}
             />
             <button
               className="filter-btn"
               name="done"
               onClick={setRender != null ? (e) => setRender(!render) : ""}
+              disabled={roleId === "12"}
             >
               FILTER
             </button>
