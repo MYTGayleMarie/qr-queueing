d234ff390d7d1e8b9b4a0f85ab52f97e660cb002
vanessa-myt <vanessa@mytdevcorp.com>
2023-11-14 15:57:22 +0800

    feat, integ: get booking details and screen for getting second file for xray

---
+++ src/components/View/Results Releasing/ViewImg.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react"
+import { useParams } from "react-router-dom"
+import { getToken, getUser, refreshPage } from "../../../utilities/Common"
+import axios from "axios"
+import ReportClinicalServices from "../Reports/ReportClinicalServices"
+
+const userToken = getToken()
+const userId = getUser()
+
+export default function ViewImg() {
+  const { type } = useParams()
+  const { bookingId } = useParams()
+  const { packageId } = useParams()
+  const { serviceId } = useParams()
+  const [base64, setBase64] = useState("")
+
+  React.useEffect(() => {
+    // Type is lab
+    if (type == "lab") {
+      axios({
+        method: "post",
+        url: window.$link + "bookings/getBookingDetails/" + bookingId,
+        withCredentials: false,
+        params: {
+          api_key: window.$api_key,
+          token: userToken.replace(/['"]+/g, ""),
+          requester: userId,
+        },
+      })
+        .then((lab) => {
+          const labDetail = lab.data.filter(
+            (details) => details.id == serviceId
+          )
+
+          setBase64(labDetail[0].file2)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+    // Type is package
+    if (type == "package") {
+      axios({
+        method: "post",
+        url: window.$link + "bookings/getBookingPackageDetails/" + packageId,
+        withCredentials: false,
+        params: {
+          api_key: window.$api_key,
+          token: userToken.replace(/['"]+/g, ""),
+          requester: userId,
+        },
+      })
+        .then((packages) => {
+          const packageDetail = packages.data.filter(
+            (details) => details.id == serviceId
+          )
+
+          setBase64(packageDetail[0].file2)
+        })
+        .catch((error) => {
+          setBase64(false)
+          console.log(error)
+        })
+    }
+  }, [])
+
+  if (base64 !== "") {
+    return <embed src={base64} className="pdfObject" />
+  } else if (base64 === false) {
+    return (
+      <div>
+        <p>Error occured while loading results. Please try again</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+}
