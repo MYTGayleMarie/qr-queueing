8087de738207a869bd1838f05f924ca239296669
vanessa-myt <vanessa@mytdevcorp.com>
2023-11-24 17:07:00 +0800

    feat: extraction api

--- src/Helpers/APIs/extractionAPI.js
+++ src/Helpers/APIs/extractionAPI.js
@@ -8,6 +8,27 @@ export const getExtractionPatients = async () => {
     params.append("api_key", window.$api_key);
     params.append("token", getToken().replace(/['"]+/g, ""));
     const response = await postAPICall(
+      window.$link + "bookings/extractionManager",
+
+      {
+        requester: getUser(),
+        api_key: window.$api_key,
+        token: getToken().replace(/['"]+/g, ""),
+      }
+    );
+
+    return { data: response.data };
+  } catch (error) {
+    return { error: error.response };
+  }
+};
+export const getLabExtractionPatients = async () => {
+  try {
+    var params = new URLSearchParams();
+    params.append("requester", getUser());
+    params.append("api_key", window.$api_key);
+    params.append("token", getToken().replace(/['"]+/g, ""));
+    const response = await postAPICall(
       window.$link + "bookings/labExtraction",
 
       {
