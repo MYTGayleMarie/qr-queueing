b51af3e3413c7e50fe7571d33e15d4ae0882d3ae
vanessa-myt <vanessa@mytdevcorp.com>
2023-11-30 16:22:51 +0800

    feat:refactor xray

--- src/components/View/XRAY/Manager.jsx
+++ src/components/View/XRAY/Manager.jsx
@@ -3,62 +3,56 @@ import Navbar from "../../Navbar"
 import Header from "../../Header"
 import Table from "../../Table.js"
 import { ToastContainer, toast } from "react-toastify"
-import {
-  getRoleId,
-  getToken,
-  getUser,
-  refreshPage,
-} from "../../../utilities/Common"
+import { getRoleId, getUser, refreshPage } from "../../../utilities/Common"
 import {
   getExtractionPatients,
+  getLabExtractionPatients,
   getXRAYExtractionPatients,
-  updateExtractionPatient,
-  updateExtractionPatientBulk,
-  updateExtractionXRAYPatientBulk,
 } from "../../../Helpers/APIs/extractionAPI"
-import { Button, ListGroup } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import { generateExtractionQueue } from "../../../Helpers/APIs/queueAPI.js"
 const buttons = []
 const patientData = ""
-const userToken = getToken()
-const userId = getUser()
-var id = ""
 
-function XrayManager() {
+const userId = getUser()
+
+function XRAYManager() {
+  const navigate = useNavigate()
   const [filteredData, setFilter] = useState({
     from_date: "",
     to_date: "",
     done: false,
   })
   const [render, setRender] = useState([])
-  const [renderDetails, setRenderDetails] = useState([])
   const [role, setRole] = useState("")
   const [records, setRecords] = useState([])
   const [selectedRow, setSelectedRow] = useState({})
-  const [recordsDetails, setRecordsDetails] = useState([])
-  const [labIds, setLabIds] = useState([])
   const [isReady, setIsReady] = useState(false)
 
   function handleExtractionClick(row) {
-    var lab_tests = row.lab_test.split("|")
-    // var lab_tests_id = row.booking_detail_id.split("|");
-
-    setSelectedRow({ ...row, lab_tests: lab_tests })
-    setRecordsDetails(lab_tests)
-    // setLabIds(lab_tests_id);
+    const data = {
+      booking_id: row.booking_id,
+      serving_type: "xray",
+      customer_id: row.customer_id,
+    }
+    generateQueue(data)
   }
 
-  async function handleUpdateBooking() {
-    const response = await updateExtractionXRAYPatientBulk(selectedRow)
+  async function generateQueue(data) {
+    const response = await generateExtractionQueue(data)
     if (response.data) {
-      toast.success(response.data.message.success.toUpperCase())
-      refreshPage()
+      toast.success(response.data.message)
+      setTimeout(() => {
+        navigate(`${data.booking_id}/${response.data.data.queue_no}`)
+      }, 2000)
     } else {
-      toast.error("Something went wrong. Please try again")
+      toast.error(response.error.data.messages.error)
       setTimeout(() => {
         refreshPage()
       }, 2000)
     }
   }
+
   async function fetchRecords() {
     const response = await getXRAYExtractionPatients()
     if (response.data) {
@@ -77,12 +71,12 @@ function XrayManager() {
         <Fragment>
           <Header
             type="thick"
-            title="XRAY MANAGER"
+            title="EXTRACTION MANAGER (ECG)"
             buttons={buttons}
             tableData={patientData}
           />
           <div className="row">
-            <div className="col-8">
+            <div className="col-12">
               <Table
                 type={"xray"}
                 onExtractionClick={handleExtractionClick}
@@ -91,7 +85,7 @@ function XrayManager() {
                 )}
                 rowsPerPage={20}
                 selectedRowExtraction={selectedRow}
-                headingColumns={["BOOKING ID", "NAME", "EXTRACTION DATE"]}
+                headingColumns={["BOOKING ID", "NAME", "EXTRACTION DATE", ""]}
                 filteredData={filteredData}
                 setFilter={setFilter}
                 setRender={setRender}
@@ -105,61 +99,6 @@ function XrayManager() {
                 isReady={isReady}
               />
             </div>
-            <div className="col-4">
-              <div className="extraction-details">
-                <div className="extraction-header">TEST</div>
-                <ListGroup defaultActiveKey="#link1">
-                  {recordsDetails.map((data, index) => {
-                    return (
-                      <ListGroup.Item className="extraction-test">
-                        <div className="row justify-content-start">
-                          <div className="col">{data}</div>
-                        </div>
-                      </ListGroup.Item>
-                    )
-                  })}
-                </ListGroup>
-                {recordsDetails.length > 0 ? (
-                  <div className="p-2">
-                    <Button
-                      className="mt-2"
-                      // size="sm"
-                      style={{
-                        width: "100%",
-                        background: "#bfbc4b",
-                        // : "#419EA3",
-                        borderColor: "#bfbc4b",
-                        // : "#419EA3",
-                      }}
-                      onClick={handleUpdateBooking}
-                    >
-                      Extracted
-                    </Button>
-                  </div>
-                ) : (
-                  <p className="p-2 text-center align-center">
-                    No Tests found.
-                  </p>
-                )}
-              </div>
-              {/* <Table
-                type={"extraction-details"}
-                tableData={recordsDetails.length > 0 ? recordsDetails : []}
-                rowsPerPage={20}
-                headingColumns={["TEST"]}
-                filteredData={filteredData}
-                setFilter={setFilter}
-                setRender={setRenderDetails}
-                render={renderDetails}
-                givenClass={"register-mobile"}
-                // link={viewBooking}
-                link={""}
-                role={role}
-                userId={userId}
-                useLoader={true}
-                isReady={isReady}
-              /> */}
-            </div>
           </div>
 
           <ToastContainer hideProgressBar={true} />
@@ -169,4 +108,4 @@ function XrayManager() {
   )
 }
 
-export default XrayManager
+export default XRAYManager
