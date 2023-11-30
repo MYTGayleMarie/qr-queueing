488c35bb73ffa5edb58b78eb72806f3edc6068ee
vanessa-myt <vanessa@mytdevcorp.com>
2023-11-30 16:21:36 +0800

    integ: single extraction, single ecg, single xray apis

--- src/Helpers/APIs/extractionAPI.js
+++ src/Helpers/APIs/extractionAPI.js
@@ -1,33 +1,53 @@
-import { getToken, getUser } from "../../utilities/Common";
-import { getAPICall, postAPICall } from "../AxiosMethodCalls";
+import { getToken, getUser } from "../../utilities/Common"
+import { getAPICall, postAPICall } from "../AxiosMethodCalls"
 
 export const getExtractionPatients = async () => {
   try {
-    var params = new URLSearchParams();
-    params.append("requester", getUser());
-    params.append("api_key", window.$api_key);
-    params.append("token", getToken().replace(/['"]+/g, ""));
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
     const response = await postAPICall(
       window.$link + "bookings/extractionManager",
-
       {
         requester: getUser(),
         api_key: window.$api_key,
         token: getToken().replace(/['"]+/g, ""),
+       
       }
-    );
-
-    return { data: response.data };
+    )
+    return { data: response.data }
   } catch (error) {
-    return { error: error.response };
+    return { error: error.response }
   }
-};
+}
+
+export const getSingleExtractionPatient = async (booking_id) => {
+  try {
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
+    const response = await postAPICall(
+      window.$link + "bookings/extractionManager/"+booking_id,
+      {
+        requester: getUser(),
+        api_key: window.$api_key,
+        token: getToken().replace(/['"]+/g, ""),
+       
+      }
+    )
+    return { data: response.data }
+  } catch (error) {
+    return { error: error.response }
+  }
+}
 export const getLabExtractionPatients = async () => {
   try {
-    var params = new URLSearchParams();
-    params.append("requester", getUser());
-    params.append("api_key", window.$api_key);
-    params.append("token", getToken().replace(/['"]+/g, ""));
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
     const response = await postAPICall(
       window.$link + "bookings/labExtraction",
 
@@ -36,19 +56,40 @@ export const getLabExtractionPatients = async () => {
         api_key: window.$api_key,
         token: getToken().replace(/['"]+/g, ""),
       }
-    );
+    )
 
-    return { data: response.data };
+    return { data: response.data }
   } catch (error) {
-    return { error: error.response };
+    return { error: error.response }
   }
-};
+}
+export const getSingleLabExtractionPatient = async (booking_id) => {
+  try {
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
+    const response = await postAPICall(
+      window.$link + "bookings/labExtraction/"+booking_id,
+
+      {
+        requester: getUser(),
+        api_key: window.$api_key,
+        token: getToken().replace(/['"]+/g, ""),
+      }
+    )
+
+    return { data: response.data }
+  } catch (error) {
+    return { error: error.response }
+  }
+}
 export const getXRAYExtractionPatients = async () => {
   try {
-    var params = new URLSearchParams();
-    params.append("requester", getUser());
-    params.append("api_key", window.$api_key);
-    params.append("token", getToken().replace(/['"]+/g, ""));
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
     const response = await postAPICall(
       window.$link + "bookings/xrayExtraction",
 
@@ -57,22 +98,43 @@ export const getXRAYExtractionPatients = async () => {
         api_key: window.$api_key,
         token: getToken().replace(/['"]+/g, ""),
       }
-    );
+    )
 
-    return { data: response.data };
+    return { data: response.data }
   } catch (error) {
-    return { error: error.response };
+    return { error: error.response }
   }
-};
-
-export const updateExtractionPatient = async (data, id) => {
+}
+export const getSingleXRAYExtractionPatients = async (booking_id) => {
   try {
-    var params = new URLSearchParams();
-    params.append("requester", getUser());
-    params.append("api_key", window.$api_key);
-    params.append("token", getToken().replace(/['"]+/g, ""));
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
     const response = await postAPICall(
-      window.$link + "Bookingdetails/updateExtraction/" + id,
+      window.$link + "bookings/xrayExtraction/"+booking_id,
+
+      {
+        requester: getUser(),
+        api_key: window.$api_key,
+        token: getToken().replace(/['"]+/g, ""),
+      }
+    )
+
+    return { data: response.data }
+  } catch (error) {
+    return { error: error.response }
+  }
+}
+
+export const updateExtractionPatient = async (data) => {
+  try {
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
+    const response = await postAPICall(
+      window.$link + "Bookingdetails/updateExtraction",
 
       {
         updated_by: getUser(),
@@ -82,19 +144,19 @@ export const updateExtractionPatient = async (data, id) => {
         status: "done",
         booking: data.booking_id,
       }
-    );
+    )
 
-    return { data: response.data };
+    return { data: response.data }
   } catch (error) {
-    return { error: error.response };
+    return { error: error.response }
   }
-};
+}
 export const updateExtractionPatientBulk = async (data) => {
   try {
-    var params = new URLSearchParams();
-    params.append("requester", getUser());
-    params.append("api_key", window.$api_key);
-    params.append("token", getToken().replace(/['"]+/g, ""));
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
     const response = await postAPICall(
       window.$link + "bookings/updateExtraction",
 
@@ -106,19 +168,19 @@ export const updateExtractionPatientBulk = async (data) => {
         // status: "done",
         booking: data.booking_id,
       }
-    );
+    )
 
-    return { data: response.data };
+    return { data: response.data }
   } catch (error) {
-    return { error: error.response };
+    return { error: error.response }
   }
-};
+}
 export const updateExtractionXRAYPatientBulk = async (data) => {
   try {
-    var params = new URLSearchParams();
-    params.append("requester", getUser());
-    params.append("api_key", window.$api_key);
-    params.append("token", getToken().replace(/['"]+/g, ""));
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
     const response = await postAPICall(
       window.$link + "bookings/updatexrayExtraction",
 
@@ -130,19 +192,19 @@ export const updateExtractionXRAYPatientBulk = async (data) => {
         // status: "done",
         booking: data.booking_id,
       }
-    );
+    )
 
-    return { data: response.data };
+    return { data: response.data }
   } catch (error) {
-    return { error: error.response };
+    return { error: error.response }
   }
-};
+}
 export const updateExtractionLabPatientBulk = async (data) => {
   try {
-    var params = new URLSearchParams();
-    params.append("requester", getUser());
-    params.append("api_key", window.$api_key);
-    params.append("token", getToken().replace(/['"]+/g, ""));
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
     const response = await postAPICall(
       window.$link + "bookings/updatelabExtraction",
 
@@ -154,10 +216,10 @@ export const updateExtractionLabPatientBulk = async (data) => {
         // status: "done",
         booking: data.booking_id,
       }
-    );
+    )
 
-    return { data: response.data };
+    return { data: response.data }
   } catch (error) {
-    return { error: error.response };
+    return { error: error.response }
   }
-};
+}
