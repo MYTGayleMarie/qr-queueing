02d69c897210a659d6bf281419aec46073faba1b
vanessa-myt <vanessa@mytdevcorp.com>
2024-01-15 16:29:58 +0800

    integ: skip

--- src/Helpers/APIs/extractionAPI.js
+++ src/Helpers/APIs/extractionAPI.js
@@ -225,3 +225,26 @@ export const updateExtractionLabPatientBulk = async (data,id) => {
     return { error: error.response }
   }
 }
+
+export const skipPatient = async (queue_id) => {
+  try {
+    var params = new URLSearchParams()
+    params.append("requester", getUser())
+    params.append("api_key", window.$api_key)
+    params.append("token", getToken().replace(/['"]+/g, ""))
+    const response = await postAPICall(
+      window.$link + "customers/skipQueue",
+
+      {
+        requester: getUser(),
+        api_key: window.$api_key,
+        token: getToken().replace(/['"]+/g, ""),
+        queue_id:queue_id
+      }
+    )
+
+    return { data: response.data }
+  } catch (error) {
+    return { error: error.response }
+  }
+}
